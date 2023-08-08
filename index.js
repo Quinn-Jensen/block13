886c2206add3642cd2fcce851d2e8c5e01435389
Quinn-Jensen <50886434+Quinn-Jensen@users.noreply.github.com>
2023-08-07 21:37:58 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,85 @@
+/*
+//Is truthy
+let truthy = "I am a string";
+const isTrue = true;
+const isFalse = false;
+const isNull = null;
+const isUndef = undefined;
+const isZero = 0;
+const isEmpty = "";
+
+//if value is truthy print true
+if (truthy === isTrue) {
+    console.log ("true");
+    //if value is falsy print the following results
+}
+if (truthy === isFalse) {
+    console.log ("The boolean value false is falsey");
+} 
+if (truthy === isNull) {
+    console.log ("The null value is falsey");
+} 
+if (truthy === isUndef) {
+    console.log ("undefined is falsey");
+}
+if (truthy === isZero) {
+    console.log ("The number 0 is falsy");
+}
+if (truthy === isEmpty) {
+    console.log ("The empty string is falsy");
+}
+*/
+
+
+/*
+// Number Line 
+// take the sum of 2 numbers and prints the results                                                                                                     
+let num1 = -1000;
+let num2 = 0;
+const sum = num1 + num2;
+
+if (sum === -1000) {
+    console.log (sum + " is a negative number" );
+} else if (sum < 0) {
+    console.log (sum + " is a negative number");
+} else if (sum === 0) {
+    console.log (sum + " is equal to 0");
+} else if (sum > 100) {
+    console.log (sum + " is greater than 100");
+} else {
+    console.log (sum + " is greater than 0");
+}
+*/
+
+/*
+// Greater than 5
+let num1 = 1000;
+let num2 = -1000;
+
+if (num1 & num2 >= 5) {
+    console.log ("True");
+} else {
+    console.log ("False");
+}
+*/
+
+
+//Pair and Compare
+//Use strict equality operator (!==) to compare 2 sets of values
+let param1A = 11;
+let param1B = "eleven";
+let param2A = "four";
+let param2B = "for";
+
+if ( param1A === param1B || param2A === param2B ) {
+    //print true if atleast one pair is truthy
+    console.log ("true");
+} else {
+    console.log ("false");
+}
+
+
+
+
+
+
